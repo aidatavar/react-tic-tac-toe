eb6da66a569d8115796dd4f41b90ebb629c1c00b
aidyel <aidayeli15@gmail.com>
2023-07-24 10:18:22 -0400

    added handleCellChange function to see who's winning

---
+++ tic-tact-toe/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Cell from "./components/Cell";
+
+
+const App = () => {
+
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", "",])
+  const [go, setGo] = useState("circle")
+  const [winningMessage, setWinningMessage] = useState(null)
+
+  const message = "It is now " + go + "'s go."
+
+  console.log(cells)
+ 
+  return (
+    <div className="app">
+      <div className="gameboard">
+        {cells.map((cell, index) => 
+        <Cell 
+          key={index} 
+          id={index} 
+          cell={cell} 
+          setCells={setCells}
+          go={go}
+          setGo={setGo}
+          cells={cells}
+        />)}
+      </div>
+
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
